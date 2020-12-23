5203081cf9a69191fd1db7d2c02f0bff778fe707
Alienmenno <alienmenno@hotmail.com>
2020-12-23 17:41:57 +0100

    day17 part2 solved

--- src/day17.rs
+++ src/day17.rs
@@ -2,13 +2,21 @@ use std::fs;
 use std::fmt;
 use std::collections::HashSet;
 
+use itertools::Itertools;
+
 pub fn solve() {
     let mut pocket_dimension = parse_file("dat/day17.txt");
-    println!("Initial state: {}", pocket_dimension);
+    let mut pocket_dimension4 = pocket_dimension.clone();
+    pocket_dimension4.add_dimension();
+    // println!("Initial state: {}", pocket_dimension);
     
     pocket_dimension = pocket_dimension.skip(5).nth(0).unwrap();
-    println!("Final state: {}", pocket_dimension);
+    // println!("Final state: {}", pocket_dimension);
     println!("iter 6: {}", pocket_dimension.activate_cubes.len());
+
+    pocket_dimension4 = pocket_dimension4.skip(5).nth(0).unwrap();
+    // println!("Final state: {}", pocket_dimension4);
+    println!("iter 6: {}", pocket_dimension4.activate_cubes.len());
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -19,70 +27,67 @@ enum CubeState {
 
 #[derive(Debug, Clone,)]
 struct PocketDimension {
-    activate_cubes: HashSet<(isize, isize, isize)>,
-    min_max_x: (isize, isize),
-    min_max_y: (isize, isize),
-    min_max_z: (isize, isize),
+    activate_cubes: HashSet<Vec<isize>>,
 }
 
 impl PocketDimension {
-    fn index(&self, x: isize, y: isize, z: isize) -> CubeState {
-        if self.activate_cubes.contains(&(x, y, z)) {CubeState::Active}
+    fn index(&self, i: &Vec<isize>) -> CubeState {
+        if self.activate_cubes.contains(i) {CubeState::Active}
         else {CubeState::Inactive}
     }
 
-    fn neighbor_count(&self, x: isize, y: isize, z:isize, state:CubeState) -> usize {
-        let neighbors: Vec<(isize, isize, isize)> = vec![
-            // z=-1
-            (-1, -1, -1), (0, -1, -1), (1, -1, -1),
-            (-1,  0, -1), (0,  0, -1), (1,  0, -1),
-            (-1,  1, -1), (0,  1, -1), (1,  1, -1),
-            // z=0
-            (-1, -1,  0), (0, -1,  0), (1, -1,  0),
-            (-1,  0,  0),              (1,  0,  0),
-            (-1,  1,  0), (0,  1,  0), (1,  1,  0),
-            // z=1
-            (-1, -1,  1), (0, -1,  1), (1, -1,  1),
-            (-1,  0,  1), (0,  0,  1), (1,  0,  1),
-            (-1,  1,  1), (0,  1,  1), (1,  1,  1),
-        ];
+    fn get_nr_dimensions(&self) -> usize {
+        self.activate_cubes.iter().nth(0).unwrap().len()
+    }
 
-        neighbors.iter()
-                 .filter(|(xu, yu, zu)| self.index(x+xu, y+yu, z+zu) == state)
+    fn get_dimension_sizes(&self) -> Vec<(isize, isize)> {
+        (0..self.get_nr_dimensions())
+            .map(|i| self.activate_cubes.iter().map(|v| v[i]).minmax().into_option().unwrap())
+            .collect()
+    }
+
+    fn add_dimension(&mut self) {
+        self.activate_cubes = self.activate_cubes
+            .iter()
+            .map(|i| {
+                let mut ni = i.clone();
+                ni.push(0);
+                ni
+            }).collect();
+    }
+
+    fn get_neighbor_indices(&self, i: &Vec<isize>) -> Vec<Vec<isize>> {
+        (0..self.get_nr_dimensions())
+            .map(|_| -1..=1)
+            .multi_cartesian_product()
+            .filter(|o| !o.iter().all(|&i| i == 0)) // ignore self index
+            // .inspect(|x| println!("{:?}", x))
+            .map(|v| v.iter().zip(i.iter()).map(|(a, b)| a+b).collect::<Vec<isize>>())
+            .collect()
+    }
+
+    fn neighbor_count(&self, i: &Vec<isize>, state:CubeState) -> usize {
+        self.get_neighbor_indices(&i).iter()
+                 .filter(|o| self.index(o) == state)
                 //  .inspect(|x| println!("{:?}", x))
                  .count()
     }
 
     fn tick(&mut self) {
-        let mut new_state: HashSet<(isize, isize, isize)> = HashSet::new();
-        let range_x = (self.min_max_x.0 - 1)..(self.min_max_x.1 + 1);
-        let range_y = (self.min_max_y.0 - 1)..(self.min_max_y.1 + 1);
-        let range_z = (self.min_max_z.0 - 1)..(self.min_max_z.1 + 1);
-
-        for (x, y, z) in iproduct!(range_x, range_y, range_z) {
-            let ct = self.index(x, y, z);
-            let anc = self.neighbor_count(x, y, z, CubeState::Active);
-            let new_cube_state = match (ct, anc) {
-                (CubeState::Active, x) if x == 2 || x == 3 => CubeState::Active,
-                (CubeState::Inactive, 3) => CubeState::Active,
-                _ => CubeState::Inactive,
-            };
-            // println!("Checking: {:?} -> {} has {} -> {}", (x, y, z), ct, anc, new_cube_state);
-
-            if new_cube_state == CubeState::Active {
-                new_state.insert((x, y, z));
-            } 
-        }
-        
-        self.min_max_x.0 = new_state.iter().min_by_key(|(x, _, _)| x).unwrap().0;
-        self.min_max_x.1 = new_state.iter().max_by_key(|(x, _, _)| x).unwrap().0+1;
-
-        self.min_max_y.0 = new_state.iter().min_by_key(|(_, y, _)| y).unwrap().1;
-        self.min_max_y.1 = new_state.iter().max_by_key(|(_, y, _)| y).unwrap().1+1;
-
-        self.min_max_z.0 = new_state.iter().min_by_key(|(_, _, z)| z).unwrap().2;
-        self.min_max_z.1 = new_state.iter().max_by_key(|(_, _, z)| z).unwrap().2+1;
-        self.activate_cubes = new_state;
+        self.activate_cubes = self.activate_cubes.iter()
+            .flat_map(|i| self.get_neighbor_indices(i))
+            .unique()
+            // .inspect(|x| println!("{:?}", x))
+            .fold(HashSet::<Vec<isize>>::new(), |mut acc, i| {
+                let ct = self.index(&i);
+                let anc = self.neighbor_count(&i, CubeState::Active);
+                match (ct, anc) {
+                    (CubeState::Active, x) if x == 2 || x == 3 => acc.insert(i),
+                    (CubeState::Inactive, 3) => acc.insert(i),
+                    _ => false,
+                };
+                acc
+            });
     }
 }
 
@@ -109,25 +114,20 @@ fn parse_file(file_name: &str) -> PocketDimension {
     
     let size_x = dimensional_cubestates.first().unwrap().len() as isize;
     let size_y = dimensional_cubestates.len() as isize;
-    let size_z = 1;
 
     PocketDimension {
-        activate_cubes: dimensional_cubestates.into_iter()
-                                              .flatten()
-                                              .enumerate()
-                                              .filter_map(|(i, s)| {
-                                                if s == CubeState::Active {
-                                                    Some((i as isize % size_x,
-                                                         (i as isize / size_x) % size_y,
-                                                         0))
-                                                }
-                                                else {
-                                                    None
-                                                }
-                                              }).collect(),
-        min_max_x: (0, size_x),
-        min_max_y: (0, size_y),
-        min_max_z: (0, size_z),
+        activate_cubes: dimensional_cubestates
+            .into_iter()
+            .flatten()
+            .enumerate()
+            .filter_map(|(i, s)| {
+            if s == CubeState::Active {
+                Some(vec![i as isize % size_x,(i as isize / size_x) % size_y,0])
+            }
+            else {
+                None
+            }
+            }).collect(),
     }
 }
 
@@ -140,19 +140,21 @@ impl fmt::Display for CubeState {
     }
 }
 
+// TODO: Do something beter for this
 impl fmt::Display for PocketDimension {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "size: (x:{:?}, y:{:?}, z:{:?})\n",
-                  self.min_max_x, self.min_max_y, self.min_max_z)?;
-        for z in self.min_max_z.0..self.min_max_z.1 {
-            write!(f, "z={}\n", z)?;
-            for y in self.min_max_y.0..self.min_max_y.1 {
-                for x in self.min_max_x.0..self.min_max_x.1 {
-                    write!(f, "{}", self.index(x, y, z))?;
-                }
-                write!(f, "\n")?;
-            }
-        }
+        let n_dimensions = self.get_nr_dimensions();
+        let dimension_sizes: Vec<(isize, isize)> = self.get_dimension_sizes();
+        write!(f, "N dimensions: {}, sizes: {:?})\n",
+                  n_dimensions, dimension_sizes)?;
+        dimension_sizes
+            .iter()
+            .map(|d| (d.0..=d.1))
+            .multi_cartesian_product()
+            .enumerate()
+            .for_each(|(i, x)| {
+                write!(f, "i: {}, ind: {:?} -> {}\n", i, x, self.index(&x)).unwrap();
+            });
         Ok(())
     }
 }
@@ -169,5 +171,17 @@ mod tests {
         pocket_dimension = pocket_dimension.skip(5).nth(0).unwrap();
         println!("Final state: {}", pocket_dimension);
         assert_eq!(pocket_dimension.activate_cubes.len(), 112);
+    }
+
+    #[ignore]
+    #[test]
+    fn test_day17_part2() {
+        let mut pocket_dimension4 = parse_file("dat/day17_example.txt");
+        pocket_dimension4.add_dimension();
+        println!("Initial state: {}", pocket_dimension4);
+        
+        pocket_dimension4 = pocket_dimension4.skip(5).nth(0).unwrap();
+        println!("Final state: {}", pocket_dimension4);
+        assert_eq!(pocket_dimension4.activate_cubes.len(), 848);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate itertools;
 extern crate regex;
 
